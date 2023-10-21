ee1ed6ce8a150f5b327d3a3df63a5a3e2a4fad8f
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-10-21 23:13:51 +0700

    error handling

--- controller/RecipeController.js
+++ controller/RecipeController.js
@@ -7,41 +7,81 @@ exports.index = async (req, res) => {
     let builder = new RecipeQueryBuilder();
     let sql = builder.getQuery(req);
 
-    return db.all(sql, [], async (err, rows) => {
-        if (err) {
-            res.status(400).json({
-                error: err.message,
-            });
-        }
-        res.json({
-            message: "success",
-            data: RecipeResource.toCollection(rows),
+    try {
+        return db.all(sql, [], async (err, rows) => {
+            if (err) {
+                res.status(500).json({
+                    error: err.message,
+                });
+                return;
+            }
+
+            try {
+                let data = RecipeResource.toCollection(rows);
+                res.json({
+                    message: "success",
+                    data: data,
+                });
+
+            } catch (err) {
+                console.log(2,err);
+                return res.status(500).json({
+                    error: err.message,
+                });
+            }
         });
-    });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
+        return;
+    }
 }
 
 exports.show = async (req, res) => {
-    db.get(`SELECT * FROM recipes where id = ?`, [req.params.id], (err, recipe) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        db.get(`SELECT * FROM users where id = ?`, [recipe.userId], (err, user) => {
+    try {
+        db.get(`SELECT * FROM recipes where id = ?`, [req.params.id], (err, recipe) => {
             if (err) {
-                res.status(400).json({"error":err.message});
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            if (!recipe) {
+                res.status(404).json({
+                    message: "not found",
+                });
                 return;
             }
-            db.all(`SELECT * FROM recipe_steps where recipeId = ?`, [req.params.id], (err, recipe_steps) => {
+            db.get(`SELECT * FROM users where id = ?`, [recipe.userId], (err, user) => {
                 if (err) {
                     res.status(400).json({"error":err.message});
                     return;
                 }
-                res.status(200).json({
-                    message: "success",
-                    data: new RecipeResource(recipe, recipe_steps),
+                db.all(`SELECT * FROM recipe_steps where recipeId = ?`, [req.params.id], (err, recipe_steps) => {
+                    if (err) {
+                        res.status(400).json({"error": err.message});
+                        return;
+                    }
+
+                    try {
+                        res.status(200).json({
+                            message: "success",
+                            data: new RecipeResource(recipe, recipe_steps),
+                        });
+
+                    } catch (err) {
+                        res.status(500).json({
+                            error: err.message,
+                        });
+                        return;
+                    }
                 });
-            });
-        })
-        
-      });
+            })
+            
+          });
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
+    }
 }
--- controller/UserController.js
+++ controller/UserController.js
@@ -3,25 +3,46 @@ const db = require("../model");
 exports.index = async (req, res) => {
     let sql = "SELECT * FROM users";
 
-    return db.all(sql, [], (err, rows) => {
-        if (err) {
-            res.status(400).json({
-                error: err.message,
+    try {
+        return db.all(sql, [], (err, rows) => {
+            if (err) {
+                res.status(400).json({
+                    error: err.message,
+                });
+            }
+            res.json({
+                message: "success",
+                data: rows,
             });
-        }
-        res.json({
-            message: "success",
-            data: rows,
         });
-    });
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
+    }
+    
 }
 
 exports.show = async (req, res) => {
-    db.get(`SELECT * FROM users where id = ?`, [req.params.id], (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.status(200).json(row);
-      });
+    try {
+        db.get(`SELECT * FROM users where id = ?`, [req.params.id], (err, row) => {
+            if (err) {
+                res.status(400).json({"error":err.message});
+                return;
+            }
+            if (!row) {
+                res.status(404).json({
+                    error: "not found",
+                });
+                return;
+            }
+            res.status(200).json(row);
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
+        return;
+    }
 }
